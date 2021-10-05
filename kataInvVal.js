8310b222a7212eb11d7125f04b2cff77aba67bd8
gary909 <garywhite61@hotmail.com>
2021-10-05 17:41:38 +0200

    code working

--- kataInvVal.js
+++ kataInvVal.js
@@ -1,5 +1,16 @@
 function invert(array) {
-    return 'hi';
+    var arr = [];
+    for (var i = 0; i < array.length; i++) {
+        if(array[i] == 0) {
+            arr.push(-0);
+        } else {
+            var temp = array[i] * -1;
+            arr.push(temp);
+        }
+    }
+    return arr;
  }
 
- console.log(invert([1,2,3,4,5]));
+ console.log(invert([0])); // returns -1,-2,-3,-4,-5
+ console.log('***********************************************'); // returns -1,-2,-3,-4,-5
+ console.log(invert([1,-2,3,-4,5])); // returns -1,2,-3,4,-5 arr.push(parseInt(-array[i]));
